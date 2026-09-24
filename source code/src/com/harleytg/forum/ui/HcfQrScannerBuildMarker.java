be1931ff2695fc99c5d6c4e1e6e5187e6eba6caf
markhitchk <mark.hitchk@gmail.com>
2026-09-23 19:41:18 -0700

    refactor(dev): migrate QR build marker to Kotlin

--- source code/src/com/harleytg/forum/ui/HcfQrScannerBuildMarker.java
+++
@@ -1,7 +0,0 @@
-package com.harleytg.forum.dev;
-
-/** Build-time marker proving the native ZXing HCF Authenticator scanner is packaged. */
-final class HcfQrScannerBuildMarker {
-    static final String VALUE = "HCF_AUTHENTICATOR_NATIVE_QR_SCANNER_V2";
-    private HcfQrScannerBuildMarker() {}
-}
