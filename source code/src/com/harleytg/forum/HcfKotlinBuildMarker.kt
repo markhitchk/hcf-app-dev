ed31473aa91f439d08ecbfcfbf99a6bb64d3065d
markhitchk <mark.hitchk@gmail.com>
2026-09-23 19:39:27 -0700

    feat(dev): add first Kotlin source

---
+++ source code/src/com/harleytg/forum/HcfKotlinBuildMarker.kt
@@ -0,0 +1,6 @@
+package com.harleytg.forum.dev
+
+/** Build-time marker proving Kotlin sources are compiled and packaged in the Beta APK. */
+internal object HcfKotlinBuildMarker {
+    const val VALUE: String = "HCF_KOTLIN_BUILD_V1"
+}
