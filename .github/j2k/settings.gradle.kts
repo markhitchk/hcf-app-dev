b01d10f14c8258d4ec8e0a7733a793ca556cc7b4
markhitchk <mark.hitchk@gmail.com>
2026-09-24 02:22:23 -0700

    ci(dev): restore JetBrains repositories for one-time J2K

--- .github/j2k/settings.gradle.kts
+++ .github/j2k/settings.gradle.kts
@@ -2,6 +2,9 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
+        maven("https://cache-redirector.jetbrains.com/plugins.jetbrains.com/maven")
+        maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
+        maven("https://cache-redirector.jetbrains.com/www.jetbrains.com/intellij-repository/releases")
         jcenter()
     }
 }
